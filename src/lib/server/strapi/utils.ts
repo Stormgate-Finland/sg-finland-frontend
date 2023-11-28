e8b2968ee531a8be88688a0be9b0f12c09aeb701
Juho Nieminen <nieminen.juho@gmail.com>
2023-11-28 23:27:17 +0200

    Support api key for strapi fetch

--- src/lib/server/strapi/utils.ts
+++ src/lib/server/strapi/utils.ts
@@ -1,5 +1,6 @@
 import { stringify } from 'qs';
 import { PUBLIC_CMS_URL } from '$env/static/public';
+import { CMS_API_KEY } from '$env/static/private';
 import type { StrapiQuery } from '$types/strapiQuery';
 
 export function strapiQuery(query?: StrapiQuery) {
@@ -12,7 +13,8 @@ export async function strapiFetch<T>(
 	defaultQuery?: StrapiQuery,
 ) {
 	const qs = strapiQuery(query ?? defaultQuery);
-	const res = await fetch(`${PUBLIC_CMS_URL}${endpoint}${qs}`);
+	const headers = CMS_API_KEY ? { authorization: `Bearer ${CMS_API_KEY}` } : undefined;
+	const res = await fetch(`${PUBLIC_CMS_URL}${endpoint}${qs}`, { headers });
 	const data = await res.json();
 	return data as T;
 }
