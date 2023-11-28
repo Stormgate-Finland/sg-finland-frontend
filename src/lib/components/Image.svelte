d032ef74168bb975aec3ad8cfb20d4b28b6e735a
Juho Nieminen <nieminen.juho@gmail.com>
2023-11-28 23:27:43 +0200

    Fix image src

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,11 +1,18 @@
-<script>
+<script lang="ts">
 	import { TwicImg } from '@twicpics/components/sveltekit';
 	import { PUBLIC_TWICPICS_DOMAIN, PUBLIC_CMS_URL } from '$env/static/public';
+
+	function formatSrc(src: string) {
+		if (src.startsWith('/')) {
+			return `${PUBLIC_CMS_URL}${$$restProps.src}`;
+		}
+		return src;
+	}
 </script>
 
 {#if PUBLIC_TWICPICS_DOMAIN}
 	<TwicImg {...$$restProps} src={$$restProps.src}></TwicImg>
 {:else}
 	<!-- svelte-ignore a11y-missing-attribute -->
-	<img {...$$restProps} src={`${PUBLIC_CMS_URL}${$$restProps.src}`} />
+	<img {...$$restProps} src={formatSrc($$restProps.src)} />
 {/if}
