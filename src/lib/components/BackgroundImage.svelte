ba7552bf4046b3499ed8fdb242a8afa662fb1cba
Juho Nieminen <nieminen.juho@gmail.com>
2024-03-19 00:49:06 +0200

    Add general background image

---
+++ src/lib/components/BackgroundImage.svelte
@@ -0,0 +1,7 @@
+<script>
+	import bgImg from '$/assets/images/stormgate-finland-bg.jpg';
+</script>
+
+<div class="pointer-events-none absolute left-0 right-0 top-0 h-dvh w-dvw opacity-5">
+	<img src={bgImg} class="fade-to-background h-full w-full object-cover" alt="" />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,14 @@
 	import SideBarLeft from '$components/SideBarLeft.svelte';
 	import SideBarRight from '$components/SideBarRight.svelte';
 	import MobileNavigation from '$components/MobileNavigation.svelte';
+	import BackgroundImage from '$components/BackgroundImage.svelte';
 
 	export let data;
 	const { siteSettings } = data;
 </script>
 
 <div class="page-gradient flex min-h-screen flex-col items-center">
+	<BackgroundImage />
 	<MobileNavigation />
 	<Header logo={siteSettings?.attributes?.logo} />
 	<div class="flex h-full w-full max-w-screen-xl grow flex-col justify-center px-2 md:px-4">
