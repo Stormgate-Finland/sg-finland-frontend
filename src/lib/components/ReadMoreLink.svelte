e4d14cd2e4f94049de7a1cde6ae22c8057b6a404
Juho Nieminen <nieminen.juho@gmail.com>
2024-03-21 00:15:27 +0200

    Fix missing read more translation

--- src/lib/components/ReadMoreLink.svelte
+++ src/lib/components/ReadMoreLink.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
+	import { t } from '$lib/translations';
 	export let href = '';
 </script>
 
-<a class="text-primary underline underline-offset-2" {href}>Read more</a>
+<a class="text-primary underline underline-offset-2" {href}>{$t('post.readMore')}</a>
