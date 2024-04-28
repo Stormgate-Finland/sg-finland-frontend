7043f9cd67a2f3211a50ec6f9a87722ca5dc708c
Juho Nieminen <nieminen.juho@gmail.com>
2024-04-28 22:43:01 +0300

    Temporary workaround for local strapi assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { loadEnv } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
 
-export default ({ mode }: { mode: string }) => {
-	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-	return defineConfig({
-		plugins: [
-			sveltekit(),
-			svg({
-				includePaths: ['./src/lib/icons/', './src/assets/icons/'],
-				svgoOptions: {
-					multipass: true,
-					plugins: [
-						{
-							name: 'preset-default',
-							// by default svgo removes the viewBox which prevents svg icons from scaling
-							// not a good idea! https://github.com/svg/svgo/pull/1461
-							params: { overrides: { removeViewBox: false } },
-						},
-					],
-				},
-			}),
-		],
-		test: {
-			include: ['src/**/*.{test,spec}.{js,ts}'],
-		},
-		server: {
-			proxy: {
-				'/uploads': {
-					target: `${process.env.CMS_URL}`,
-				},
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		svg({
+			includePaths: ['./src/lib/icons/', './src/assets/icons/'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						// by default svgo removes the viewBox which prevents svg icons from scaling
+						// not a good idea! https://github.com/svg/svgo/pull/1461
+						params: { overrides: { removeViewBox: false } },
+					},
+				],
+			},
+		}),
+	],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+	server: {
+		proxy: {
+			'/uploads': {
+				// Temp workaround to make local strapi assets work
+				target: 'http://localhost:1337',
 			},
 		},
-	});
-};
+	},
+});
