a7fe94bddd99ded7194a57b850963fec5a778581
Juho Nieminen <nieminen.juho@gmail.com>
2024-05-06 21:53:42 +0300

    Show only live streams, no offline streams

--- src/routes/streams/+page.server.ts
+++ src/routes/streams/+page.server.ts
@@ -1,26 +1,26 @@
-import { getLiveStreams, getStreams } from '$lib/server/strapi/streams';
+import { getLiveStreams } from '$lib/server/strapi/streams';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ url }) => {
-	const page = Number(url.searchParams.get('page')) || 1;
-	const pageSize = Number(url.searchParams.get('limit')) || 20;
+export const load: PageServerLoad = async () => {
+	// const page = Number(url.searchParams.get('page')) || 1;
+	// const pageSize = Number(url.searchParams.get('limit')) || 20;
 
-	const [liveStreams = [], streams] = await Promise.all([
+	const [liveStreams = []] = await Promise.all([
 		getLiveStreams(),
-		getStreams({
-			populate: { tag: { fields: ['name'] } },
-			pagination: { page, pageSize },
-			sort: ['title:asc'],
-		}),
+		// getStreams({
+		// 	populate: { tag: { fields: ['name'] } },
+		// 	pagination: { page, pageSize },
+		// 	sort: ['title:asc'],
+		// }),
 	]);
 
 	return {
 		liveStreams,
-		streams: {
-			...streams,
-			...(liveStreams?.length > 0 && {
-				data: streams.data.filter((stream) => !liveStreams.some((s) => s.id === Number(stream.id))),
-			}),
-		},
+		// streams: {
+		// 	...streams,
+		// 	...(liveStreams?.length > 0 && {
+		// 		data: streams.data.filter((stream) => !liveStreams.some((s) => s.id === Number(stream.id))),
+		// 	}),
+		// },
 	};
 };
